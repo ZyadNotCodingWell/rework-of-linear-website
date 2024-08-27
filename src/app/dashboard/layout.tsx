f0fa8c8ea7f0cfbfe5722a9c0080db6cc67c4f9a
ZyadNotCodingWell <zyaddch@gmail.com>
2024-08-27 22:01:01 +0200

    msg deleted:    src/app/dashboard/layout.tsx

--- src/app/dashboard/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-// import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "a title",
-  description: "This is questionnable actually",
-};
