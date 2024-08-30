617b03faf781a1826872b1968f42c53d541fc2be
ZyadNotCodingWell <zyaddch@gmail.com>
2024-08-30 15:16:27 +0200

    why do i have to name all my commits, it's just a ctrl + s tbh modified:   src/app/components/CustomerSection.tsx

--- src/app/components/CustomerSection.tsx
+++ src/app/components/CustomerSection.tsx
@@ -9,13 +9,13 @@ export default function CustomerSection() {
 	
 	return (
 	<div className="container self-center mx-auto w-full justify-center">
-	<div id="Customers-Section" className=" max-w-96 md:max-w-2xl flex flex-col lg:min-w-96 flex-none md:px-20 self-center mx-auto py-4">
+	<div id="Customers-Section" className=" max-w-96 md:max-w-6xl flex flex-col lg:min-w-96 flex-none md:px-20 self-center mx-auto py-4">
         
 		<div id="customers-text" className="text-md md:text-2xl text-white text-center mx-auto">Powering the world&apos;s best product teams.</div>
 		<div className="text-xs md:text-lg text-white/70 text-center mx-auto">From next-gen startups to established entreprises.</div>
 		
 		{/*Tablet and desktop version of logo section*/}
-		<div className="relative mt-12 hidden md:flex md:flex-col w-full text-2xl font-semibold gap-4 px-4" onMouseEnter={() => setBlurredDiv(true)} onMouseLeave={() => setBlurredDiv(false)}>	
+		<div className="relative mt-8 py-4 hidden md:flex md:flex-col w-full text-2xl font-semibold gap-4 px-4" onMouseEnter={() => setBlurredDiv(true)} onMouseLeave={() => setBlurredDiv(false)}>	
 			<div className="flex flex-row items-center justify-between"> 
 				<div className="flex justify-center items-center min-w-40 text-center mx-auto">Brand A</div>
 				<div className="flex justify-center items-center min-w-40 text-center mx-auto">Brand B</div>
@@ -26,9 +26,9 @@ export default function CustomerSection() {
 				<div className="flex justify-center items-center min-w-40 text-center mx-auto">Brand E</div>
 				<div className="flex justify-center items-center min-w-40 text-center mx-auto">Brand F</div>
 			</div>	
-			<div className={blurredDiv ? "absolute flex inset-0 w-full items-center backdrop-blur-xl z-40 transition duration-300" : "hidden"} >
+			<div className={blurredDiv ? "absolute flex inset-0 w-full items-center justify-center bg-black/55 backdrop-blur-xl z-40 transition duration-300" : "hidden"} >
 					<Link href="#">
-						<div className="text-xl font-normal min-w-2xl self-center mx-auto"> Meet our customers</div>
+						<div className="text-xl lg:text-3xl bg-neutral-900 px-4 py-2 rounded-xl lg:rounded-2xl border border-white/30 font-normal min-w-2xl self-center mx-auto"> Meet our customers</div>
 					</Link>
 			</div>
 		</div>
@@ -53,6 +53,7 @@ export default function CustomerSection() {
 				<div className="flex justify-center items-center min-w-40 text-center mx-auto">Brand E</div>
 				<div className="flex justify-center items-center min-w-40 text-center mx-auto">Brand F</div>
 			</motion.div>
+			{/* End of phone version of logo autoscroller */}
 		</div>
 
 
